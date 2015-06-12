2ed85ebf7ee48e6c48e69d3f9dbe3eade1b3a0d2
Martin J <martinnjorgensen@gmail.com>
2015-06-12 19:21:51 +0200

    Card class should be done for now.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,24 +1,66 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 /// <summary>
 /// Class representing a playable card in the game.
 /// </summary>
-public class Card {
+public class Card : MonoBehaviour
+{
 
+    /***********************
+     * Attributes and data *
+     ***********************/
+
+    #region "Looks"
+    /// <summary>
+    /// String containing the name or title of the card.
+    /// </summary>
+    public string CardName;
+
+    /// <summary>
+    /// String containing the description/quote from the card.
+    /// </summary>
+    public string CardDescription;
+
+    /// <summary>
+    /// Image containing the cards monicker (area that contains strength).
+    /// </summary>
+    public UnityEngine.UI.Image CardMonicker;
+
+    /// <summary>
+    /// Image with card protrait.
+    /// </summary>
+    public UnityEngine.UI.Image CardPortrait;
+
+    /// <summary>
+    /// Image containing the cards background look.
+    /// </summary>
+    public UnityEngine.UI.Image CardBackground;
+    #endregion
+
+    #region "Stats & Attributes"
     /// <summary>
     /// Main Type for the card, decides where the card goes on the board.
     /// </summary>
-    public readonly Enums.CardType Type;
+    public Enums.CardType Type;
 
     /// <summary>
     /// Unordered list of special abilities attached to the card. Most cards will have 0 or 1.
     /// </summary>
-    public readonly List<Enums.CardAbility> Abilities;
+    public List<Enums.CardAbility> Abilities;
 
     /// <summary>
     /// Field used for weather type, can be null if the CardType is not set to Weather.
     /// </summary>
-    public readonly Enums.WeatherType WeatherType;
+    public Enums.WeatherType WeatherType;
+
+    /// <summary>
+    /// The base strength of the card before any modifiers.
+    /// </summary>
+    public int BaseStrength;
+    #endregion
+
+
 }
