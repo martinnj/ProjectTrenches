17a058ddb08b9e6ba2b057e9213edcc1617e288b
Martin J <martinnjorgensen@gmail.com>
2015-06-12 18:15:23 +0200

    Added some base work.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class representing a playable card in the game.
+/// </summary>
+public class Card {
+
+    /// <summary>
+    /// Main Type for the card, decides where the card goes on the board.
+    /// </summary>
+    public readonly Enums.CardType Type;
+
+    /// <summary>
+    /// Unordered list of special abilities attached to the card. Most cards will have 0 or 1.
+    /// </summary>
+    public readonly List<Enums.CardAbility> Abilities;
+
+    /// <summary>
+    /// Field used for weather type, can be null if the CardType is not set to Weather.
+    /// </summary>
+    public readonly Enums.WeatherType WeatherType;
+}
---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+/// <summary>
+/// Different enumerables used for the game.
+/// </summary>
+public class Enums {
+
+    /// <summary>
+    /// Enum to represent where on the board this card belongs.
+    /// </summary>
+    public enum CardType
+    {
+        Infantry, // Front row.
+        Ranged,   // Mid row.
+        Siege,    // Rear row.
+        Weather,  // Weather/environment box.
+        Morale    // Goes next to a row.
+    };
+
+    /// <summary>
+    /// Abilies that can be attached to cards.
+    /// </summary>
+    public enum CardAbility
+    {
+        Hero,       // Heroes cannot interact with special cards or abilities.
+        Spy,        // Play to opponents area, take 2 random cards from your deck.
+        TightBond,  // For each card with same name in row, double strength.
+        Horde,      // Play any card with same name from your deck.
+        Revive,     // Take a card from discard pile, play at once.
+        Diversion,  // Replace a card on your battlefield and return it to hand.
+        RowBuff     // Buff all cards in the row with +1 strength.
+    };
+
+    /// <summary>
+    /// Different kind of weather cards.
+    /// </summary>
+    public enum WeatherType
+    {
+        Frost, // Reduce all infrantry to base strength 1.
+        Fog,   // Reduce all ranged to base strength 1.
+        Rain,  // Reduce all siege to base strength 1.
+        Shine  // Disspell all active weather cards.
+    };
+}
